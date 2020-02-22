8fa99346283de652b522f48d1e06edd30145add7
d22aldana@gmail.com <d22aldana@gmail.com>
2020-02-22 11:30:15 -0400

    Creacion de web api completa

---
+++ solUno/webapiUno/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapiUno.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre completo")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        public string Name { get; set; }
+
+        [Display(Name = "Carnet de identidad")]
+        public string Document { get; set; }
+
+    }
+}
